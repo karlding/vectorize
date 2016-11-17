ecada10e2ca043110fbc480b941db46582428dda
Karl <karlding@users.noreply.github.com>
2016-11-17 02:18:32 -0500

    Add distance function to utils
    Add preliminary distance function, using basic Root Mean Squared

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,14 +6,16 @@ from __future__ import unicode_literals
 
 from PIL import Image
 import os
-import vectorize
+
 from vectorize import utils
+from vectorize.utils import average_image_colour
 
 import py.test
 
 def test_average_image_colour_white():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     image = Image.open(os.path.join(os.path.sep, dir_path, "white.png"))
+    image = image.convert('RGBA')
     average_colour = utils.average_image_colour(image)
     assert average_colour[0] == 255
     assert average_colour[1] == 255
@@ -22,7 +24,24 @@ def test_average_image_colour_white():
 def test_average_image_colour_black():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     image = Image.open(os.path.join(os.path.sep, dir_path, "black.png"))
+    image = image.convert('RGBA')
     average_colour = utils.average_image_colour(image)
     assert average_colour[0] == 0
     assert average_colour[1] == 0
     assert average_colour[2] == 0
+
+def test_image_rms_diff_same_image():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    image = Image.open(os.path.join(os.path.sep, dir_path, "black.png"))
+    image = image.convert('RGBA')
+
+    assert utils.image_rms_diff(image, image) == 0
+
+def test_image_rms_diff():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    black = Image.open(os.path.join(os.path.sep, dir_path, "black.png"))
+    black = black.convert('RGBA')
+    white = Image.open(os.path.join(os.path.sep, dir_path, "white.png"))
+    white = white.convert('RGBA')
+
+    assert utils.image_rms_diff(black, white) == 1
--- vectorize/utils.py
+++ vectorize/utils.py
@@ -1,5 +1,12 @@
 """Utility functions used across Vectorize"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from PIL import Image
+
+import math
 
 def average_image_colour(image):
     image = image.convert('RGBA')
@@ -7,6 +14,7 @@ def average_image_colour(image):
     green = 0
     blue = 0
     count = 0
+
     for r, g, b, a in image.getdata():
         red = red + r
         green = green + g
@@ -14,3 +22,18 @@ def average_image_colour(image):
         count = count + 1
     return int(red / count), int(green / count), int(blue / count)
 
+def image_rms_diff(src, dest):
+    """Calculate the difference between two images using root mean squared"""
+    total = 0
+    width, height = src.size
+    for x in range(width):
+        for y in range(height):
+            s_r, s_g, s_b, s_a = src.getpixel((x, y))
+            d_r, d_g, d_b, d_a = dest.getpixel((x, y))
+
+            diff_r = s_r - d_r
+            diff_g = s_g - d_g
+            diff_b = s_b - d_b
+
+            total = total + (diff_r ** 2) + (diff_g ** 2) + (diff_b ** 2)
+    return math.sqrt(total / (width * height * 3)) / 255
